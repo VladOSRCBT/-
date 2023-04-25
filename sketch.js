ce17ea5b1cb8ea6af7f7d246b1e328063e4cdd92
vlad ivanov <simple1213@yandex.ru>
2023-04-25 22:23:04 +0700

    Добавил курсор

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
 function setup () {
 createCanvas(windowWidth, windowHeight);
-
 }
 
 function draw () {
 background(100);
+ellipse(mouseX, mouseY, 21, 21);
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
 }
